3d31eaa8d66d57ff408499681ef7f4cde3b37a0e
kavx1626 <161681146+kavx1626@users.noreply.github.com>
2025-06-26 16:02:13 +0530

    Add files via upload

--- port.js
+++ port.js
@@ -1,3 +1,16 @@
  function toggleTheme() {
       document.body.classList.toggle('dark-mode');
     }
+const animatedSections = document.querySelectorAll('.section-animate');
+
+function showOnScroll() {
+  animatedSections.forEach(section => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top <= window.innerHeight * 0.8) {
+      section.classList.add('visible');
+    }
+  });
+}
+
+window.addEventListener('scroll', showOnScroll);
+window.addEventListener('load', showOnScroll);
